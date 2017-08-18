e6eba89ffd7c4aa66c25bac6840afc8590ad752d
Neha Batra <nehalita@gmail.com>
2017-08-17 22:06:58 -0700

    Change hello world to Create Profile as a react Button div

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Button() {
+  return <button id="btn">Create Profile</button>
+}
+
+ReactDOM.render(<Button />, window.root)
--- test/features/indexTest.js
+++ test/features/indexTest.js
@@ -3,17 +3,43 @@ import {
   navigate,
   findElement,
   getText,
-  close
+  click,
+  close,
+  fillIn,
+  getAttribute
 } from 'selenium-adapter'
 import test from 'ava'
 
-test('welcomes the user', async t => {
-  const chrome = makeHeadlessChromeAdapter()
+test.beforeEach(t => {
+  t.context.chrome = makeHeadlessChromeAdapter()
+})
+
+test.afterEach.always(async t => {
+  await close(t.context.chrome)
+})
+
+test('I can create a profile with my name', async t => {
+  const chrome = t.context.chrome
   await navigate(chrome, 'http://localhost:3000')
 
-  const p = await findElement(chrome, 'p')
-  const welcomeText = await getText(p)
-  await close(chrome)
-
-  t.is(welcomeText, 'Hello World!')
+  const createProfileButton = await findElement(
+    chrome,
+    'button',
+    'Create Profile'
+  )
+  t.is(await getText(createProfileButton), 'Create Profile')
+  // await click(createProfileButton)
+  //
+  // const nameInput = await findElement(chrome, 'input[name="name"]')
+  // t.is(await getAttribute(nameInput, 'name'), 'name')
+  // await fillIn(nameInput, 'Sally Speaker')
+  //
+  // const submitProfileButton = await findElement(chrome, 'button', 'Submit')
+  // t.is(await getText(submitProfileButton), 'Submit')
+  // await click(submitProfileButton)
+  //
+  // const profileNameDiv = await findElement(chrome, 'div.profile .name')
+  // const profileName = await getText(profileNameDiv)
+  //
+  // t.is(profileName, 'Sally Speaker')
 })
